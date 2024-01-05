bc75d89bcf2295374a53d12cd71359138659ca7b
Bor Kokovnik <bor.kokovnik@gmail.com>
2024-01-05 15:24:47 +0100

    Upogib linearni fit

--- FP3/Upogib/Upogib.py
+++ FP3/Upogib/Upogib.py
@@ -46,25 +46,180 @@ Ux2 = np.array([m_k, m_k + m_1, m_k + m_1 + m_2, m_k + m_1 + m_2 + m_3,  m_k + m
 Uy2_1 = np.array([7.370, 7.345, 7.295, 7.250, 7.110, 6.830, 6.340, 5.780, 4.320, 1.430, 4.255, 5.625, 6.190, 6.725, 6.980, 7.125, 7.160, 7.250, 7.265]) #
 Uy2_2 = np.array([9.285, 9.260, 9.220, 9.190, 9.100, 8.930, 8.585, 8.260, 7.405, 5.690, 7.400, 8.255, 8.590, 8.925, 9.100, 9.190, 9.220, 9.255, 9.270]) #
 
+Uy2_1_err = unumpy.uarray(Uy2_1, np.ones(np.shape(Uy2_1)) * 0.005)
+Uy2_2_err = unumpy.uarray(Uy2_2, np.ones(np.shape(Uy2_2)) * 0.005)
+
+
 # Sila mikrometra
 d = np.array([9.365, 8.295, 7.260, 4.970, 3.275, 2.750])
 F = np.array([104, 87, 91, 74, 67, 71])
 
+d_err = unumpy.uarray(d, np.array([0.005 for i in d]))
+F_err = unumpy.uarray(F, np.array([0.005 for i in F]))
+
 
 ##### Konstante, parametri in začetne vrednosti
-print(mase)
+# print(mase)
 
 
 ##### Definicije funkcij
-def fit_fun2(x, a, b):
-    return a * x + b
+# def fit_fun2(x, a, b):
+#     return a * x + b
+
+def fit_fun2(B, x):
+    '''Linear function y = m*x + b'''
+    # B is a vector of the parameters.
+    # x is an array of the current x values.
+    # x is in the same format as the x passed to Data or RealData.
+    #
+    # Return an array in the same format as y passed to Data or RealData.
+    return B[0]*x + B[1]
 
 
 ##### Obdelava seznamov
-plt.plot(d, F)
+# plt.plot(d, F)
 # plt.plot(unumpy.nominal_values(Ux2), Uy2_1)
 # plt.plot(unumpy.nominal_values(Ux2), Uy2_2)
+# plt.show()
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.odr import *
+
+
+############# Mikrometrska ura
+
+# Initiate some data, giving some randomness using random.random().
+x_mik = unumpy.nominal_values(d_err)
+y_mik = unumpy.nominal_values(F_err)
+
+x_err_mik = unumpy.std_devs(d_err)
+y_err_mik = unumpy.std_devs(F_err)
+
+# Create a model for fitting.
+lin_model_mik = Model(fit_fun2)
+
+# Create a RealData object using our initiated data from above.
+data_mik = RealData(x_mik, y_mik, sx=x_err_mik, sy=y_err_mik)
+
+# Set up ODR with the model and data.
+odr_mik = ODR(data_mik, lin_model_mik, beta0=[0., 1.])
+
+# Run the regression.
+out_mik = odr_mik.run()
+
+# Use the in-built pprint method to give us results.
+out_mik.pprint()
+'''Beta: [ 1.01781493  0.48498006]
+Beta Std Error: [ 0.00390799  0.03660941]
+Beta Covariance: [[ 0.00241322 -0.01420883]
+ [-0.01420883  0.21177597]]
+Residual Variance: 0.00632861634898189
+Inverse Condition #: 0.4195196193536024
+Reason(s) for Halting:
+  Sum of squares convergence'''
+
+x_fit_mik = np.linspace(x_mik[0], x_mik[-1], 1000)
+y_fit_mik = fit_fun2(out_mik.beta, x_mik)
+
+plt.errorbar(x_mik, y_mik, xerr=x_err_mik, yerr=y_err_mik, linestyle='None', marker='x')
+plt.plot(x_mik, y_fit_mik)
+
 plt.show()
+
+print(out_mik.beta)
+
+
+
+
+
+
+############# Okrogla palica
+
+# Initiate some data, giving some randomness using random.random().
+x_ok = unumpy.nominal_values(Ux2)
+y_ok = unumpy.nominal_values(Uy2_1_err)
+
+x_err_ok = unumpy.std_devs(Ux2)
+y_err_ok = unumpy.std_devs(Uy2_1_err)
+
+# Create a model for fitting.
+quad_model_ok = Model(fit_fun2)
+
+# Create a RealData object using our initiated data from above.
+data_ok = RealData(x_ok, y_ok, sx=x_err_ok, sy=y_err_ok)
+
+# Set up ODR with the model and data.
+odr_ok = ODR(data_ok, quad_model_ok, beta0=[0., 1.])
+
+# Run the regression.
+out_ok = odr_ok.run()
+
+# Use the in-built pprint method to give us results.
+out_ok.pprint()
+'''Beta: [ 1.01781493  0.48498006]
+Beta Std Error: [ 0.00390799  0.03660941]
+Beta Covariance: [[ 0.00241322 -0.01420883]
+ [-0.01420883  0.21177597]]
+Residual Variance: 0.00632861634898189
+Inverse Condition #: 0.4195196193536024
+Reason(s) for Halting:
+  Sum of squares convergence'''
+
+x_fit_ok = np.linspace(x_ok[0], x_ok[-1], 1000)
+y_fit_ok = fit_fun2(out_ok.beta, x_ok)
+
+plt.errorbar(x_ok, y_ok, xerr=x_err_ok, yerr=y_err_ok, linestyle='None', marker='x')
+plt.plot(x_ok, y_fit_ok)
+
+plt.show()
+
+print(out_ok.beta)
+
+
+
+############ Kvadratna palica
+
+# Initiate some data, giving some randomness using random.random().
+x_kv = unumpy.nominal_values(Ux2)
+y_kv = unumpy.nominal_values(Uy2_2_err)
+
+x_err_kv = unumpy.std_devs(Ux2)
+y_err_kv = unumpy.std_devs(Uy2_2_err)
+
+# Create a model for fitting.
+quad_model_kv = Model(fit_fun2)
+
+# Create a RealData object using our initiated data from above.
+data_kv = RealData(x_kv, y_kv, sx=x_err_kv, sy=y_err_kv)
+
+# Set up ODR with the model and data.
+odr_kv = ODR(data_kv, quad_model_kv, beta0=[0., 1.])
+
+# Run the regression.
+out_kv = odr_kv.run()
+
+# Use the in-built pprint method to give us results.
+out_kv.pprint()
+'''Beta: [ 1.01781493  0.48498006]
+Beta Std Error: [ 0.00390799  0.03660941]
+Beta Covariance: [[ 0.00241322 -0.01420883]
+ [-0.01420883  0.21177597]]
+Residual Variance: 0.00632861634898189
+Inverse Condition #: 0.4195196193536024
+Reason(s) for Halting:
+  Sum of squares convergence'''
+
+x_fit_kv = np.linspace(x_kv[0], x_kv[-1], 1000)
+y_fit_kv = fit_fun2(out_kv.beta, x_kv)
+
+plt.errorbar(x_kv, y_kv, xerr=x_err_kv, yerr=y_err_kv, linestyle='None', marker='x')
+plt.plot(x_kv, y_fit_kv)
+
+plt.show()
+
+print(out_kv.beta)
 
 
 
