4008e2c2d096506f9f525113c6ffed66a353966a
Bor Kokovnik <bor.kokovnik@gmail.com>
2024-05-28 19:22:27 +0200

    Začetek SevCT

--- FP4/SevCT/SevCT.py
+++ FP4/SevCT/SevCT.py
@@ -39,6 +39,14 @@ def obtezeno_povprecje(uarray):
 
     return unc.ufloat(obtezeno_povprecje, obtezena_negotovost)
 
+def fit_fun(B, x):
+    a = 1.1 / (8.617 * 10 ** (-5))
+    return (1 - 15 / (np.pi) ** 4 * (-(a / x) ** 3 * np.log(1 - np.e ** ( - (a / x))) + (6 + 6 * (a / x) + 3 * (a / x) ** 2) * np.e ** (- (a / x)))) * B[0]
+
+def fit_fun1(b, x):
+    a = 1.1 / (8.617 * 10 ** (-5))
+    return (1 - 15 / (np.pi) ** 4 * (-(a / x) ** 3 * np.log(1 - np.e ** ( - (a / x))) + (6 + 6 * (a / x) + 3 * (a / x) ** 2) * np.e ** (- (a / x)))) * b
+
 def lin_fun(x, a, b):
     return a * x + b
 
@@ -89,7 +97,7 @@ def fit_napake(x: unp.uarray, y: unp.uarray, funkcija=lin_fun2, print=0) -> np.n
 
 def fit_napake_x(x: unp.uarray, y: unp.uarray, funkcija=lin_fun2, print=0) -> tuple:
     '''Sprejme 2 unumpy arraya skupaj z funkcijo in izračuna fit, upoštevajoč y napake'''
-    optimizedParameters, pcov = opt.curve_fit(lin_fun, unp.nominal_values(x), unp.nominal_values(y))#, sigma=unp.std_devs(y), absolute_sigma=True)
+    optimizedParameters, pcov = opt.curve_fit(funkcija, unp.nominal_values(x), unp.nominal_values(y))#, sigma=unp.std_devs(y), absolute_sigma=True)
     return (optimizedParameters, pcov)
 
 def graf_errorbar(x: unp.uarray, y: unp.uarray, podatki_label="Izmerjeno"):
@@ -121,7 +129,7 @@ def graf_fit(x: unp.uarray, y: unp.uarray, fit: np.ndarray, fit_label="Fit"):
     x_mik = unp.nominal_values(x)
     y_mik = unp.nominal_values(y)
 
-    x_fit_mik = np.linspace(x_mik[0] - x_mik[0] / 2, x_mik[-1] + x_mik[-1] / 2, 1000)
+    x_fit_mik = np.linspace(np.min(x_mik) - np.abs(np.min(x_mik)) / 2, np.max(x_mik) + np.abs(np.max(x_mik)), 5000)
 
     if type(fit) is tuple:
         y_fit_mik = lin_fun(fit[0][0], fit[0][1], x_fit_mik)
@@ -158,3 +166,122 @@ def graf_oblika(Naslov: str, x_os: str, y_os: str, legenda=1):
     plt.title(Naslov, y=1.02)
 
 #####################################################################################################################################################
+
+P_el = unp.uarray([31.7, 30, 28.5, 27, 25.5, 24, 22.5, 21, 19.5, 18, 16.5, 15, 13.5, 12, 10.5, 9, 7.5, 6, 4.5, 3, 1.5, 0],
+                  np.ones(np.shape(np.array([31.7, 30, 28.5, 27, 25.5, 24, 22.5, 21, 19.5, 18, 16.5, 15, 13.5, 12, 10.5, 9, 7.5, 6, 4.5, 3, 1.5, 0]))) * 0.2)
+
+U = unp.uarray([230, 223, 215, 208, 200, 193, 185, 177.2, 168, 159.1, 150.5, 142.3, 132.8, 123.1, 113.1, 101.6, 90.7, 78.1, 63.9, 49.5, 31.6, 0],
+               np.ones(np.shape(np.array([230, 223, 215, 208, 200, 193, 185, 177.2, 168, 159.1, 150.5, 142.3, 132.8, 123.1, 113.1, 101.6, 90.7, 78.1, 63.9, 49.5, 31.6, 0]))) * 0.3)
+
+I = unp.uarray([137.3, 134.7, 132.2, 129.7, 127, 124.6, 121.9, 119.3, 115.8, 112.9, 109.1, 105.9, 102.1, 98.1, 93.7, 88.5, 83.3, 77, 69.3, 60.9, 48.8, 0],
+               np.ones(np.shape(np.array([137.3, 134.7, 132.2, 129.7, 127, 124.6, 121.9, 119.3, 115.8, 112.9, 109.1, 105.9, 102.1, 98.1, 93.7, 88.5, 73.3, 77, 69.3, 60.9, 48.8, 0]))) * 0.2) * 10**(-3)
+
+P_m = unp.uarray([2, 1.89, 1.78, 1.68, 1.58, 1.49, 1.39, 1.28, 1.18, 1.07, 0.965, 0.875, 0.769, 0.679, 0.583, 0.481, 0.390, 0.298, 0.209, 0.144, 0.060, 0.03],
+                 np.ones(np.shape(np.array([2, 1.89, 1.78, 1.68, 1.58, 1.49, 1.39, 1.28, 1.18, 1.07, 0.965, 0.875, 0.769, 0.679, 0.583, 0.481, 0.390, 0.298, 0.209, 0.144, 0.060, 0.03]))) * 0.02) * 10**(-3)
+
+P_m = P_m - P_m[-1]
+
+P_cel = (4 * np.pi * (unc.ufloat(34, 0.5) + unc.ufloat(1.95, 0.01)) ** 2) * P_m / 1
+
+graf_errorbar(P_el, P_cel, "Neovirano")
+
+
+U_ok = unp.uarray([230.2, 222.2, 215.2, 208, 200, 192.6, 184.6, 177, 168.4, 160, 151.0, 142, 132, 123, 113, 102, 91, 78, 65, 49, 32, 0],
+                  np.ones(np.shape(np.array([230, 223, 215, 208, 200, 193, 185, 177.2, 168, 159.1, 150.5, 142.3, 132.8, 123.1, 113.1, 101.6, 90.7, 78.1, 63.9, 49.5, 31.6, 0]))) * 0.3)
+
+I_ok = unp.uarray([137.7, 134.9, 132.7, 130, 127.2, 124.6, 121.7, 119.0, 115.9, 112.7, 109.4, 105.8, 101.9, 98.0, 93.7, 88.7, 83.3, 77.1, 69.8, 60.6, 48.8, 0],
+                  np.ones(np.shape(np.array([137.3, 134.7, 132.2, 129.7, 127, 124.6, 121.9, 119.3, 115.8, 112.9, 109.1, 105.9, 102.1, 98.1, 93.7, 88.5, 73.3, 77, 69.3, 60.9, 48.8, 0]))) * 0.2) * 10**(-3)
+
+P_m_ok = unp.uarray([605, 591, 583, 573, 542, 520, 493, 469, 430, 404, 375, 346, 302, 268, 232, 190, 152, 117, 75, 35, 0, -70],
+                    np.ones(np.shape(np.array([2, 1.89, 1.78, 1.68, 1.58, 1.49, 1.39, 1.28, 1.18, 1.07, 0.965, 0.875, 0.769, 0.679, 0.583, 0.481, 0.390, 0.298, 0.209, 0.144, 0.060, 0.03]))) * 3) * 10**(-6)
+
+P_m_ok = P_m_ok - P_m_ok[-1]
+
+P_cel_ok = (4 * np.pi * (unc.ufloat(34, 0.5) + unc.ufloat(1.95, 0.01)) ** 2) * P_m_ok / 1
+
+
+
+graf_errorbar(P_el, P_cel_ok, "Skozi silicijevo okno")
+
+x_limits = plt.xlim()
+y_limits = plt.ylim()
+
+graf_oblika(r"Oddana moč žarnice v odvisnosti od električne moči", r"$P_{el}$ [W]", r"$P_{oddana}$ [W]")
+
+plt.ylim(y_limits)
+plt.xlim(x_limits)
+
+plt.savefig(r"FP4\SevCT\Slike\Moc.pdf", bbox_inches='tight', pad_inches=0.1)
+plt.show()
+
+
+
+### Upornost nitke v odvisnosti od temperature
+
+P_0 = 30
+T_0 = 2700
+
+T_x = (P_cel / P_0) ** (1/4) * T_0
+
+T_x_s = np.concatenate((T_x[0:-1], unp.uarray([22.8 + 273], [0.1])))
+
+R = np.concatenate((U[0:-1] / I[0:-1], unp.uarray([121.5], [0.5])))
+
+graf_errorbar(T_x_s, R)
+
+x_limits = plt.xlim()
+y_limits = plt.ylim()
+plt.ylim(y_limits)
+plt.xlim(x_limits)
+
+fit_R_T = fit_napake(T_x_s, R)
+
+graf_fit(T_x_s, R, fit_R_T)
+
+graf_oblika(r"Upornost nitke v odvisnosti od temperature", r"$T$ [K]", r"$R$ [$\Omega$]")
+
+plt.savefig(r"FP4\SevCT\Slike\Upornost.pdf", bbox_inches='tight', pad_inches=0.1)
+plt.show()
+
+
+
+
+### Zadnji del vaje
+
+graf_errorbar(T_x[0:-2], P_cel_ok[0:-2] / P_cel[0:-2])
+
+x_limits = plt.xlim()
+y_limits = plt.ylim()
+plt.ylim(y_limits)
+plt.xlim(x_limits)
+
+
+plt.plot(unp.nominal_values(T_x)[0:-1], fit_fun(np.array([1]), unp.nominal_values(T_x)[0:-1]), label="Model brez odbojnosti")
+
+# optimizedParameters, pcov = opt.curve_fit(fit_fun1, unp.nominal_values(T_x[0:-2]), unp.nominal_values(P_cel_ok[0:-2] / P_cel[0:-2]), sigma=unp.std_devs(P_cel_ok[0:-2] / P_cel[0:-2]), absolute_sigma=True)
+
+# print(optimizedParameters)
+
+# plt.plot(unp.nominal_values(T_x)[0:-1], fit_fun(np.array([1.1 / (8.617 * 10 ** (-5)), 1]), unp.nominal_values(T_x)[0:-1]) * optimizedParameters[0])
+
+# plt.plot(unp.nominal_values(T_x), fit_fun1(unp.nominal_values(T_x), *fit))
+
+fit_P = fit_napake(T_x[0:-2], P_cel_ok[0:-2] / P_cel[0:-2], fit_fun)
+
+x_fit_mik = np.linspace(np.min(unp.nominal_values(T_x)[0:-2]) - np.abs(np.min(unp.nominal_values(T_x)[0:-2])) / 2, np.max(unp.nominal_values(T_x)[0:-2]) + np.abs(np.max(unp.nominal_values(T_x)[0:-2])), 5000)
+
+print(fit_P.beta, fit_P.sd_beta)
+
+y_fit_mik = fit_fun(fit_P.beta, x_fit_mik)
+plt.plot(x_fit_mik, y_fit_mik, "--", label="Model z odbojnostjo", color="#5e5e5e", linewidth=1)
+
+nu = unc.ufloat(fit_P.beta[0], fit_P.sd_beta[0])
+
+# plt.plot(unp.nominal_values(T_x)[0:-1], fit_fun(np.array([2 * 3.54 / (1 + 3.54 ** 2)]), unp.nominal_values(T_x)[0:-1]))
+
+print((2 + (4 - 4 * nu ** 2) ** (0.5)) / (2 * nu))
+
+graf_oblika("Delež prepuščene moči skozi silicijevo steklo\nvodvisnosti od temperature", r"$T$ [K]", r"$P_{Si} / P$")
+
+plt.savefig(r"FP4\SevCT\Slike\Prepusceno.pdf", bbox_inches='tight', pad_inches=0.1)
+plt.show()
